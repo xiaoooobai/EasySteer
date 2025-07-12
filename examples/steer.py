7512d3f0c827fe6e1ce845a22eb94f3fff582aa5
ZJUMXY <3210104399@zju.edu.cn>
2025-07-12 17:50:58 +0800

    close debug,output base

--- examples/steer.py
+++ examples/steer.py
@@ -24,7 +24,7 @@ steer_vector_request_pos = SteerVectorRequest(
             prefill_trigger_tokens="-1",
             prefill_trigger_positions=[-1],
             generate_trigger_tokens="-1",
-            debug=True,
+            debug=False,
             algorithm='direct'
         )
 steer_vector_request_neg = SteerVectorRequest(
@@ -35,7 +35,7 @@ steer_vector_request_neg = SteerVectorRequest(
             prefill_trigger_tokens="-1",
             prefill_trigger_positions=[-1],
             generate_trigger_tokens="-1",
-            debug=True,
+            debug=False,
             algorithm='direct'
         )
 sampling_params = SamplingParams(temperature=0.0,max_tokens=1280)
@@ -43,6 +43,13 @@ prompt_template = "<|im_start|>user\n%s<|im_end|>\n<|im_start|>assistant\n"
 # prompt = "Find the constant term in the expansion of $$\\left(10x^3-\\frac{1}{2x^2}\\right)^{5}$$"
 prompt = "Darrell and Allen's ages are in the ratio of 7:11. If their total age now is 162, calculate Allen's age 10 years from now."
 input = prompt_template % prompt
+llm = LLM(model=model_path, enable_steer_vector=True, tensor_parallel_size=1)
+output_base = llm.generate(
+    input,
+    sampling_params
+)
+generated_text_base = output_base[0].outputs[0].text
+
 llm = LLM(model=model_path, enable_steer_vector=True, tensor_parallel_size=1)
 output_pos = llm.generate(
                 input,
@@ -65,6 +72,8 @@ timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 filename = f'thinking_switch_GSM8K_{timestamp}.txt'
 
 with open(filename, 'w', encoding='utf-8') as f:
+    f.write("=== Base ===\n")
+    f.write(generated_text_base + "\n\n")
     f.write("=== Positive: Slow thinking ===\n")
     f.write(generated_text_pos + "\n\n")
     f.write("=== Negative: Fast thinking ===\n")
